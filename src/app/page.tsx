0e2e92c7a165e1a9c7e5cfdb28a87219ec789698
lukasmiljic <smiljicluka13@gmail.com>
2024-06-27 22:52:48 +0200

    add simple home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import logo from "../../public/images/medusa_logo.svg"
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello, world!</h1>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Image src={logo} className="w-48 mb-2" alt="logo" />
+        <p className="text-secondary">Simple storefront made with Next.js</p>
+        <Link className="text-xs text-[#7c3aed]" href={"/products"}> &gt; View all products</Link>
+      </div>
     </main>
   );
 }
