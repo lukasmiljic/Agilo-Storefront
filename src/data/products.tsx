f387666cdd30999a5d21fb26c6fc81ade9b38600
lukasmiljic <smiljicluka13@gmail.com>
2024-06-27 22:36:12 +0200

    modify test data to fit product info component

--- src/data/products.tsx
+++ src/data/products.tsx
@@ -2,23 +2,23 @@ const products = [
   {
     id: 1,
     title: "Shirt 1",
-    price: 100,
+    price: "100,00 kr",
     description: "This is a description of Product 1",
     image:
       "https://www.mrporter.com/variants/images/3633577411310824/in/w2000_q60.jpg",
     stock: 10,
     colors: ["red", "blue", "green"],
     sizes: ["S", "M", "L", "XL"],
-    materials: "Cotton 100%",
+    details: {title: "Materials", description: "Cotton 100%"}
   },
   {
     id: 2,
     title: "Shirt 2",
-    price: 100,
+    price: "102,00 kr",
     description: "This is a description of Product 1",
     image: "https://img-c.udemycdn.com/course/750x422/5444528_d4e3_5.jpg",
     stock: 10,
-    colors: ["red", "blue", "green"],
+    colors: ["Red", "Blue", "Green"],
     materials: "Cotton 100%",
   },
 ];
