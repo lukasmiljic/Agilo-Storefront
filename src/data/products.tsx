df449c4b0a5fbf0df1a86c83b8615de2515005f1
lukasmiljic <smiljicluka13@gmail.com>
2024-06-27 21:07:45 +0200

    add temp testing data

---
+++ src/data/products.tsx
@@ -0,0 +1,26 @@
+const products = [
+  {
+    id: 1,
+    title: "Shirt 1",
+    price: 100,
+    description: "This is a description of Product 1",
+    image:
+      "https://www.mrporter.com/variants/images/3633577411310824/in/w2000_q60.jpg",
+    stock: 10,
+    colors: ["red", "blue", "green"],
+    sizes: ["S", "M", "L", "XL"],
+    materials: "Cotton 100%",
+  },
+  {
+    id: 2,
+    title: "Shirt 2",
+    price: 100,
+    description: "This is a description of Product 1",
+    image: "https://img-c.udemycdn.com/course/750x422/5444528_d4e3_5.jpg",
+    stock: 10,
+    colors: ["red", "blue", "green"],
+    materials: "Cotton 100%",
+  },
+];
+
+export default products;
