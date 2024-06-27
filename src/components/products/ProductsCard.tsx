ac54bfeabd5edeb1640cc3a69118a624b378dea1
lukasmiljic <smiljicluka13@gmail.com>
2024-06-27 21:08:12 +0200

    add product card component

---
+++ src/components/products/ProductsCard.tsx
@@ -0,0 +1,16 @@
+const ProductCard = ({ title, image }: { title: string; image: string }) => {
+  return (
+    <div className="relative flex aspect-square cursor-pointer bg-slate-100">
+      <div className="flex h-full w-full items-center justify-center">
+        <img
+          src={image}
+          alt={title}
+          className="max-h-full max-w-full object-contain"
+        />
+      </div>
+      <p className="max-w-9/10 absolute bottom-2 left-3">{title}</p>
+    </div>
+  );
+};
+
+export default ProductCard;
