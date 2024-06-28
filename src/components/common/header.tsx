3f09ee8e91e70726c3c73a93db768431eb7348fd
lukasmiljic <smiljicluka13@gmail.com>
2024-06-28 22:07:32 +0200

    remove filter button from header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,16 +1,14 @@
-import { GoFilter } from "react-icons/go";
 import logo from "../../../public/images/medusa_logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full bg-slate-100">
-      <div className="container m-auto flex h-16 flex-row items-center justify-between px-4">
+    <div className="w-full">
+      <div className="container m-auto flex h-16 flex-row items-center justify-center px-4">
         <Link href="/">
           <Image src={logo} alt="medusa" className="cursor-pointer" />
         </Link>
-        <GoFilter />
       </div>
     </div>
   );
