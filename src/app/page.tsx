de3344b207a2f76a9a09ac773115b1df5ecb4466
lukasmiljic <smiljicluka13@gmail.com>
2024-06-28 14:41:30 +0200

    add fade in animation to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-screen animate-fadeIn">
         <Image src={logo} className="w-48 mb-2" alt="logo" />
         <p className="text-secondary">Simple storefront made with Next.js</p>
         <Link className="text-xs text-[#7c3aed]" href={"/products"}> &gt; View all products</Link>
