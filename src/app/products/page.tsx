1ccf9bbcad4cf6a27cde654c34fb74260e0d4ee9
lukasmiljic <smiljicluka13@gmail.com>
2024-06-28 22:11:32 +0200

    add sidebar component to products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import Sidebar from "@/components/UI/Sidebar";
 import ProductsGrid from "@/components/products/ProductsGrid";
+import { useState } from "react";
 
 const ProductsPage = () => {
+  const [productFilter, setProductFilter] = useState([] as string[]);
+
   return (
     <div>
-      <ProductsGrid />
+      <Sidebar
+        productFilter={productFilter}
+        setProductFilter={setProductFilter}
+      />
+      <ProductsGrid productFilter={productFilter} />
     </div>
   );
 };
